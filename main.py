b78eb67d9839a2fcb60b1db4f1f122d7a955e118
jnichols1995 <157620914+jnichols1995@users.noreply.github.com>
2024-02-07 15:35:10 +0000

    installed matplotlib commit

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import numpy as np 
 
+import matplotlib.pyplot as plt
+
 a = np.arange(15).reshape(3,5)
 
 print(a)
+
+print(a.shape)
+print(a.dtype.name)
+
+a = np.array([2,3,4])
+print(a.max)
+print(a)
+
+
+plt.plot([1,2,3,4])
+plt.ylabel('some numbers')
+plt.show()
+
+
+
+
 
 
 
